119aa5d3b0834546099d63d32b2d27aa008dcf37
noobprofesor <noobprofesor@gmail.com>
2025-02-13 13:29:49 +0300

    initial commit

---
+++ src/main/java/com/snpbot/snpbot/SnpBotApplication.java
@@ -0,0 +1,13 @@
+package com.snpbot.snpbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snpbot/snpbot/SnpBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snpbot.snpbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
